75fb49deed32a97a14aac8370474c595970f91f4
logarithm <iamrohan04@gmail.com>
2018-12-31 19:13:22 +0530

    tree

---
+++ Tree/Binary Tree/Creation.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+//It is imcomplete
+
+struct BinaryTreeNode
+{
+    int data;
+    BinaryTreeNode *leftSubTree;
+    BinaryTreeNode *rightSubTree;
+};
+
+int main()
+{
+
+    return 0;
+}
