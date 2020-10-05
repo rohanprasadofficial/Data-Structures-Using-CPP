067b5a1e241b6c115f8a16b3600cf0931d295f75
SHWETARCH <shwetabhsaxena99@gmail.com>
2020-10-05 13:30:15 +0530

    add graph algorithms

---
+++ Graph/Bellman Ford's Algorithm.cpp
@@ -0,0 +1,29 @@
+vector <int> v [2000 + 10];
+    int dis [1000 + 10];
+
+    for(int i = 0; i < m + 2; i++){
+
+        v[i].clear();
+        dis[i] = 2e9;
+    }
+
+   for(int i = 0; i < m; i++){
+
+        scanf("%d%d%d", &from , &next , &weight);
+
+        v[i].push_back(from);
+        v[i].push_back(next);
+        v[i].push_back(weight);
+   }
+
+    dis[0] = 0;
+    for(int i = 0; i < n - 1; i++){
+        int j = 0;
+        while(v[j].size() != 0){
+
+            if(dis[ v[j][0]  ] + v[j][2] < dis[ v[j][1] ] ){
+                dis[ v[j][1] ] = dis[ v[j][0]  ] + v[j][2];
+            }
+            j++;
+        }
+    }
